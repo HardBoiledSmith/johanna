6a7cc010a395e52be7e4ff70e515f89f339e2bd2
HIPERCUBE <joowonryoo@gmail.com>
2016-09-28 13:00:20 +0000

    open sample config

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit(0)
     
-    config = json.loads(open('config.json').read())
+    config = json.loads(open('config.json.sample').read())
 
     # AWS ACCESS KEY ID
     config['aws']['AWS_ACCESS_KEY_ID'] = args.accesskey
