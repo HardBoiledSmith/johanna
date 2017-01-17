7ed29bd618985d8709adc03dfb3d74aa2e2be656
Jesang Yoon <yoonjs2@gmail.com>
2017-01-18 02:46:56 +0900

    Terminate queue all at once

--- run_create_sqs.py
+++ run_create_sqs.py
@@ -5,22 +5,17 @@ import json
 
 from env import env
 from run_common import AWSCli
-from run_common import print_message
 from run_common import print_session
-
-args = []
 
 if __name__ == "__main__":
     from run_common import parse_args
 
-    args = parse_args()
+    parse_args()
 
 aws_cli = AWSCli()
 
 
 def run_create_queue(name, settings):
-    print_message('create ' + name)
-
     attr = dict()
     attr['VisibilityTimeout'] = settings['TIMEOUT']
     attr['MessageRetentionPeriod'] = settings['RETENTION']
@@ -32,6 +27,8 @@ def run_create_queue(name, settings):
     cmd += ['--attributes', json.dumps(attr)]
     aws_cli.run(cmd)
 
+    print('create : ' + queue)
+
 
 ################################################################################
 #
@@ -41,15 +38,6 @@ def run_create_queue(name, settings):
 print_session('create sqs')
 
 ################################################################################
-
-queue_list = env['sqs']
-if len(args) == 2:
-    target_eb_name = args[1]
-    for queue in queue_list:
-        if queue['NAME'] == target_eb_name:
-            run_create_queue(queue['NAME'], queue)
-            break
-    print('"%s" is not exists in config.json' % target_eb_name)
-else:
-    for queue in queue_list:
-        run_create_queue(queue['NAME'], queue)
+queues = env['sqs']
+for queue in queues:
+    run_create_queue(queue['NAME'], queue)
