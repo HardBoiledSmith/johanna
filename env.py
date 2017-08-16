26b39dc8f70db5524230c9ad765fbdd42e18412a
Jesang Yoon <yoonjs2@gmail.com>
2017-08-16 18:21:02 +0900

    GEN-768: Add multi app support for same bucket

--- env.py
+++ env.py
@@ -1,5 +1,9 @@
 import json
 import sys
 
+config_path = '.'
+if sys.path[0]:
+    config_path = sys.path[0]
+
 # Load Config.json
-env = json.load(open('%s/config.json' % sys.path[0]))
+env = json.load(open('%s/config.json' % config_path))
