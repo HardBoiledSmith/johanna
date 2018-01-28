548080eb3e0101f62d87e30de7fae7ea50b33bae
Jesang Yoon <yoonjs2@gmail.com>
2018-01-28 22:26:42 +0900

    GEN-518 GEN-519 GEN-1466 Daily CD ready for lambda

--- run_create_lambda.py
+++ run_create_lambda.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import os
-import subprocess
 import time
 
 from env import env
@@ -8,9 +6,8 @@ from run_common import AWSCli
 from run_common import check_template_availability
 from run_common import print_message
 from run_common import print_session
-from run_common import re_sub_lines
-from run_common import read_file
-from run_common import write_file
+from run_create_lambda_cron import run_create_lambda_cron
+from run_create_lambda_default import run_create_lambda_default
 
 args = []
 
@@ -51,156 +48,6 @@ def create_iam_for_lambda():
         time.sleep(120)
 
 
-def run_create_default_lambda(name, settings):
-    description = settings['DESCRIPTION']
-    function_name = settings['NAME']
-    phase = env['common']['PHASE']
-    template_name = env['template']['NAME']
-
-    template_path = 'template/%s' % template_name
-    deploy_folder = '%s/lambda/%s' % (template_path, name)
-
-    ################################################################################
-    print_session('create lambda: %s' % function_name)
-
-    gitignore_path = '%s/.gitignore' % deploy_folder
-    if os.path.exists(gitignore_path):
-        ll = read_file(gitignore_path)
-        print_message('cleanup generated files')
-        subprocess.Popen(' '.join(['rm', '-rf'] + ll), shell=True, cwd=deploy_folder).communicate()
-
-    print_message('install dependencies')
-
-    requirements_path = '%s/requirements.txt' % deploy_folder
-    if os.path.exists(requirements_path):
-        print_message('install dependencies')
-
-        cmd = ['pip3', 'install', '-r', requirements_path, '-t', deploy_folder]
-        subprocess.Popen(cmd).communicate()
-
-    settings_path = '%s/settings_local_sample.py' % deploy_folder
-    if os.path.exists(settings_path):
-        print_message('create environment values')
-
-        lines = read_file(settings_path)
-        option_list = list()
-        option_list.append(['PHASE', phase])
-        for key in settings:
-            value = settings[key]
-            option_list.append([key, value])
-        for oo in option_list:
-            lines = re_sub_lines(lines, '^(%s) .*' % oo[0], '\\1 = \'%s\'' % oo[1])
-        write_file('%s/settings_local.py' % deploy_folder, lines)
-
-    print_message('zip files')
-
-    cmd = ['zip', '-r', 'deploy.zip', '.']
-    subprocess.Popen(cmd, cwd=deploy_folder).communicate()
-
-    print_message('create lambda function')
-
-    role_arn = aws_cli.get_role_arn('aws-lambda-default-role')
-
-    cmd = ['lambda', 'create-function',
-           '--function-name', function_name,
-           '--description', description,
-           '--zip-file', 'fileb://deploy.zip',
-           '--role', role_arn,
-           '--handler', 'lambda.handler',
-           '--runtime', 'python3.6',
-           '--timeout', '120']
-    aws_cli.run(cmd, cwd=deploy_folder)
-
-
-def run_create_cron_lambda(name, settings):
-    description = settings['DESCRIPTION']
-    function_name = settings['NAME']
-    phase = env['common']['PHASE']
-    schedule_expression = settings['SCHEDULE_EXPRESSION']
-    template_name = env['template']['NAME']
-
-    template_path = 'template/%s' % template_name
-    deploy_folder = '%s/lambda/%s' % (template_path, name)
-
-    ################################################################################
-    print_session('create lambda: %s' % function_name)
-
-    gitignore_path = '%s/.gitignore' % deploy_folder
-    if os.path.exists(gitignore_path):
-        ll = read_file(gitignore_path)
-        print_message('cleanup generated files')
-        subprocess.Popen(' '.join(['rm', '-rf'] + ll), shell=True, cwd=deploy_folder).communicate()
-
-    requirements_path = '%s/requirements.txt' % deploy_folder
-    if os.path.exists(requirements_path):
-        print_message('install dependencies')
-
-        cmd = ['pip3', 'install', '-r', requirements_path, '-t', deploy_folder]
-        subprocess.Popen(cmd).communicate()
-
-    settings_path = '%s/settings_local_sample.py' % deploy_folder
-    if os.path.exists(settings_path):
-        print_message('create environment values')
-
-        lines = read_file(settings_path)
-        option_list = list()
-        option_list.append(['PHASE', phase])
-        for key in settings:
-            value = settings[key]
-            option_list.append([key, value])
-        for oo in option_list:
-            lines = re_sub_lines(lines, '^(%s) .*' % oo[0], '\\1 = \'%s\'' % oo[1])
-        write_file('%s/settings_local.py' % deploy_folder, lines)
-
-    print_message('zip files')
-
-    cmd = ['zip', '-r', 'deploy.zip', '.']
-    subprocess.Popen(cmd, cwd=deploy_folder).communicate()
-
-    print_message('create lambda function')
-
-    role_arn = aws_cli.get_role_arn('aws-lambda-default-role')
-
-    cmd = ['lambda', 'create-function',
-           '--function-name', function_name,
-           '--description', description,
-           '--zip-file', 'fileb://deploy.zip',
-           '--role', role_arn,
-           '--handler', 'lambda.handler',
-           '--runtime', 'python3.6',
-           '--timeout', '120']
-    result = aws_cli.run(cmd, cwd=deploy_folder)
-
-    function_arn = result['FunctionArn']
-
-    print_message('create cron event')
-
-    cmd = ['events', 'put-rule',
-           '--name', function_name + 'CronRule',
-           '--description', description,
-           '--schedule-expression', schedule_expression]
-    result = aws_cli.run(cmd)
-
-    rule_arn = result['RuleArn']
-
-    print_message('give event permission')
-
-    cmd = ['lambda', 'add-permission',
-           '--function-name', function_name,
-           '--statement-id', function_name + 'StatementId',
-           '--action', 'lambda:InvokeFunction',
-           '--principal', 'events.amazonaws.com',
-           '--source-arn', rule_arn]
-    aws_cli.run(cmd)
-
-    print_message('link event and lambda')
-
-    cmd = ['events', 'put-targets',
-           '--rule', function_name + 'CronRule',
-           '--targets', '{"Id" : "1", "Arn": "%s"}' % function_arn]
-    aws_cli.run(cmd)
-
-
 ################################################################################
 #
 # start
@@ -221,10 +68,10 @@ if len(args) == 2:
         if lambda_env['NAME'] == target_lambda_name:
             target_lambda_name_exists = True
             if lambda_env['TYPE'] == 'default':
-                run_create_default_lambda(lambda_env['NAME'], lambda_env)
+                run_create_lambda_default(lambda_env['NAME'], lambda_env)
                 break
             if lambda_env['TYPE'] == 'cron':
-                run_create_cron_lambda(lambda_env['NAME'], lambda_env)
+                run_create_lambda_cron(lambda_env['NAME'], lambda_env)
                 break
             print('"%s" is not supported' % lambda_env['TYPE'])
             raise Exception()
@@ -233,10 +80,10 @@ if len(args) == 2:
 else:
     for lambda_env in _lambda:
         if lambda_env['TYPE'] == 'default':
-            run_create_default_lambda(lambda_env['NAME'], lambda_env)
+            run_create_lambda_default(lambda_env['NAME'], lambda_env)
             continue
         if lambda_env['TYPE'] == 'cron':
-            run_create_cron_lambda(lambda_env['NAME'], lambda_env)
+            run_create_lambda_cron(lambda_env['NAME'], lambda_env)
             continue
         print('"%s" is not supported' % lambda_env['TYPE'])
         raise Exception()
