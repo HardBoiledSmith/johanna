fcd219bcf0723107ef4ae7dfac951adad61757d1
Jongwon(Jerry) Han <addnull@gmail.com>
2018-03-16 17:36:51 +0900

    GEN-1576 separate code related to IAM from VPC

--- run_terminate_vpc.py
+++ run_terminate_vpc.py
@@ -10,65 +10,6 @@ if __name__ == "__main__":
     parse_args()
 
 
-def terminate_iam():
-    ################################################################################
-    #
-    # IAM
-    #
-    ################################################################################
-    print_session('terminate iam')
-
-    aws_cli = AWSCli()
-
-    ################################################################################
-    print_message('terminate iam: aws-elasticbeanstalk-service-role')
-
-    cmd = ['iam', 'detach-role-policy']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-service-role']
-    cmd += ['--policy-arn', 'arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'detach-role-policy']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-service-role']
-    cmd += ['--policy-arn', 'arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'delete-role']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-service-role']
-    aws_cli.run(cmd, ignore_error=True)
-
-    ################################################################################
-    print_message('terminate iam: aws-elasticbeanstalk-ec2-role')
-
-    cmd = ['iam', 'detach-role-policy']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-ec2-role']
-    cmd += ['--policy-arn', 'arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'detach-role-policy']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-ec2-role']
-    cmd += ['--policy-arn', 'arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'detach-role-policy']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-ec2-role']
-    cmd += ['--policy-arn', 'arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'remove-role-from-instance-profile']
-    cmd += ['--instance-profile-name', 'aws-elasticbeanstalk-ec2-role']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-ec2-role']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'delete-role']
-    cmd += ['--role-name', 'aws-elasticbeanstalk-ec2-role']
-    aws_cli.run(cmd, ignore_error=True)
-
-    cmd = ['iam', 'delete-instance-profile']
-    cmd += ['--instance-profile-name', 'aws-elasticbeanstalk-ec2-role']
-    aws_cli.run(cmd, ignore_error=True)
-
-
 def main(settings):
     aws_cli = AWSCli(settings['AWS_DEFAULT_REGION'])
     rds_subnet_name = env['rds']['DB_SUBNET_NAME']
@@ -361,5 +302,3 @@ print_session('terminate vpc')
 ################################################################################
 for vpc_env in env['vpc']:
     main(vpc_env)
-
-terminate_iam()
