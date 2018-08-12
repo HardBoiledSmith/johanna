f1fa0996e49b4de6ca6a43f1351e4df531d551c8
Jongwon(Jerry) Han <addnull@gmail.com>
2018-08-13 00:03:39 +0900

    trivial change

--- run_create_lambda_sns.py
+++ run_create_lambda_sns.py
@@ -34,7 +34,7 @@ def run_create_lambda_sns(name, settings):
         region, topic_name = sns_topic_name.split('/')
         topic_arn = AWSCli(region).get_topic_arn(topic_name)
         if not topic_arn:
-            print('sns topic: "%s" is not exists in %s' % (settings['SNS_TOPIC_NAME'], region))
+            print('sns topic: "%s" is not exists in %s' % (sns_topic_name, region))
             raise Exception()
         topic_arn_list.append(topic_arn)
 
