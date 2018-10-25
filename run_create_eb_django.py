36410ebf9d70d6d242c442012a8ce73d7ca0ec75
Jesang Yoon <yoonjs2@gmail.com>
2018-10-25 14:45:47 +0900

    (in-progress) fix eb creation error

--- run_create_eb_django.py
+++ run_create_eb_django.py
@@ -163,7 +163,9 @@ def run_create_eb_django(name, settings):
         git_command = ['git', 'clone', '--depth=1', git_url]
     else:
         git_command = ['git', 'clone', '--depth=1', '-b', phase, git_url]
-    result, error = subprocess.Popen(git_command, cwd=environment_path).communicate()
+    print('git_command:', git_command)
+    result, error = subprocess.Popen(git_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                     cwd=environment_path).communicate()
     print('result:', result)
     print('error:', error)
     if not os.path.exists('%s/%s' % (environment_path, name)):
