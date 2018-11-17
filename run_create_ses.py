5a02d8afa7ce2ed4c186bd0d0e7fe2325e414444
changhoonhyun <ckdekf369@gmail.com>
2018-11-17 17:14:30 +0900

    GEN-3034 add ses notification settings

--- run_create_ses.py
+++ run_create_ses.py
@@ -2,9 +2,10 @@
 import json
 
 from env import env
-from run_common import check_template_availability
-from run_common import print_session
 from run_common import AWSCli
+from run_common import check_template_availability
+from run_common import print_message
+from run_common import print_session
 
 aws_cli = AWSCli('us-east-1')
 ses = env['ses']
@@ -14,10 +15,46 @@ def create_email_identity():
     cmd = ['ses', 'list-identities']
     identities_results = dict(aws_cli.run(cmd))
 
-    for email in ses['EMAILS']:
-        if email not in identities_results['Identities']:
+    for identity in ses['IDENTITIES']:
+        if identity['EMAIL'] not in identities_results['Identities']:
             cmd = ['ses', 'verify-email-identity',
-                   '--email-address', email]
+                   '--email-address', identity['EMAIL']]
+            aws_cli.run(cmd)
+
+            sns_topic_name = identity['SNS_TOPICS_NAME']
+            print_message('check topic exists: %s' % sns_topic_name)
+            region, topic_name = sns_topic_name.split('/')
+            topic_arn = AWSCli(region).get_topic_arn(topic_name)
+            if not topic_arn:
+                print('sns topic: "%s" is not exists in %s' % (sns_topic_name, region))
+                raise Exception()
+
+            cmd = ['ses', 'set-identity-notification-topic',
+                   '--identity', identity['EMAIL'],
+                   '--notification-type', 'Bounce',
+                   '--sns-topic', topic_arn
+                   ]
+            aws_cli.run(cmd)
+
+            cmd = ['ses', 'set-identity-headers-in-notifications-enabled',
+                   '--identity', identity['EMAIL'],
+                   '--notification-type', 'Bounce',
+                   '--enabled'
+                   ]
+            aws_cli.run(cmd)
+
+            cmd = ['ses', 'set-identity-notification-topic',
+                   '--identity', identity['EMAIL'],
+                   '--notification-type', 'Complaint',
+                   '--sns-topic', topic_arn
+                   ]
+            aws_cli.run(cmd)
+
+            cmd = ['ses', 'set-identity-headers-in-notifications-enabled',
+                   '--identity', identity['EMAIL'],
+                   '--notification-type', 'Complaint',
+                   '--enabled'
+                   ]
             aws_cli.run(cmd)
 
 
