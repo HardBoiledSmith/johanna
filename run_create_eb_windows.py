d78a3660f2abbc994d322a9f20de7c4d8ba3349e
Jesang Yoon <yoonjs2@gmail.com>
2019-08-14 22:38:46 +0900

    Fix artifact path

--- run_create_eb_windows.py
+++ run_create_eb_windows.py
@@ -158,7 +158,7 @@ def run_create_eb_windows(name, settings):
     file_name = '%s-gendo-%s.zip' % (branch, git_hash_app.decode('utf-8').strip())
     artifact_url = url + '/%s' % file_name
 
-    cmd = ['s3', 'cp', artifact_url, '%s/%s/gendo-artifact.zip' % (template_path, name)]
+    cmd = ['s3', 'cp', artifact_url, '%s/gendo-artifact.zip' % name]
     aws_cli.run(cmd, cwd=template_path)
 
     ################################################################################
