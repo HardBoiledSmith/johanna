3371541a6f31898633ad1b592fcde33ff7ce4b34
rkgusrnrlrl <rkdgusrnrlrl@gmail.com>
2019-10-16 15:59:30 +0900

    GEN-6157 Add Configuration Sets sns Destination

--- run_create_sns.py
+++ run_create_sns.py
@@ -23,6 +23,15 @@ def run_create_sns_topic(name, settings):
     result = aws_cli.run(cmd)
     print('created:', result['TopicArn'])
 
+    if 'EMAIL' in settings:
+        print_message('create sns topic(%s) email subscribe' % name)
+        cmd = ['sns', 'subscribe']
+        cmd += ['--topic-arn', result['TopicArn']]
+        cmd += ['--protocol', 'email']
+        cmd += ['--notification-endpoint', settings['EMAIL']]
+        aws_cli.run(cmd)
+
+
 
 ################################################################################
 #
