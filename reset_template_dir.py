b153d53592f15a9a511be511efa81bda1863b9c7
rkgusrnrlrl <rkdgusrnrlrl@gmail.com>
2019-12-10 10:17:49 +0900

    GEN-6672 Fix argument check and wrong how to use

--- reset_template_dir.py
+++ reset_template_dir.py
@@ -7,7 +7,7 @@ from run_common import reset_template_dir
 def _print_how_to():
     print('please input path')
     print('(sample)')
-    print('\t./generate_config.py dv-variable.json')
+    print('\t./reset_template_dir.py config_git_url template_dir_name phase')
 
 
 if __name__ == "__main__":
@@ -15,5 +15,9 @@ if __name__ == "__main__":
     git_url = sys.argv[1]
     template_name = sys.argv[2]
     phase = sys.argv[3]
+
+    if len(sys.argv) != 4:
+        _print_how_to()
+        exit(1)
 
     reset_template_dir(git_url, template_name, phase)
