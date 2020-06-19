32a50ab2a28259ca7c20066852046f9232f62ed2
Jihwan Kim <noneobj@gmail.com>
2020-06-19 20:17:53 +0900

    DEV-9417 Divide reset_template_dir and run_reset_template

--- run_reset_template.py
+++ run_reset_template.py
@@ -1,3 +1,33 @@
-from run_common import reset_template_dir
+#!/usr/bin/env python3
+import os
+import subprocess
 
-reset_template_dir()
+from env import env
+from run_common import print_message
+from run_common import print_session
+
+template_name = env['template']['NAME']
+print_session('reset template dir: %s' % template_name)
+
+print_message('cleanup existing template')
+
+if os.path.exists('template/'):
+    subprocess.Popen(['rm', '-rf', './template'], cwd='template').communicate()
+
+subprocess.Popen(['mkdir', '-p', './template']).communicate()
+
+git_url = env['template']['GIT_URL']
+name = env['template']['NAME']
+phase = env['common']['PHASE']
+
+print_message('download template from git repository')
+
+if phase == 'dv':
+    template_git_command = ['git', 'clone', '--depth=1', git_url]
+else:
+    template_git_command = ['git', 'clone', '--depth=1', '-b', phase, git_url]
+
+subprocess.Popen(template_git_command, cwd='template').communicate()
+
+if not os.path.exists('template/' + name):
+    raise Exception()
