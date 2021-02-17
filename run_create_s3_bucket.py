760a561ad535d3bbe386b98b067649466ba550e6
Jesang Yoon <yoonjs2@gmail.com>
2021-02-17 13:59:00 +0900

    Fix broken bucket setup

--- run_create_s3_bucket.py
+++ run_create_s3_bucket.py
@@ -26,7 +26,7 @@ def run_create_s3_bucket(name, settings):
 
     ################################################################################
 
-    if policy in ['website', 'email']:
+    if policy in ['website', 'email', 'temp-bucket']:
         print_message('delete public access block')
 
         cmd = ['s3api', 'delete-public-access-block']
