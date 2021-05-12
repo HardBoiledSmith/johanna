344d5dbb1e7d490541d57e2ebc17940b50036ab5
Jesang Yoon <yoonjs2@gmail.com>
2021-05-13 01:39:32 +0900

    update eb platform version

--- run_create_eb_django_al2.py
+++ run_create_eb_django_al2.py
@@ -419,7 +419,7 @@ def run_create_eb_django_al2(name, settings):
     cmd += ['--cname-prefix', cname]
     cmd += ['--environment-name', eb_environment_name]
     cmd += ['--option-settings', option_settings]
-    cmd += ['--solution-stack-name', '64bit Amazon Linux 2 v3.2.1 running Python 3.8']
+    cmd += ['--solution-stack-name', '64bit Amazon Linux 2 v3.2.2 running Python 3.8']
     cmd += ['--tags', tag0, tag1]
     cmd += ['--version-label', eb_environment_name]
     aws_cli.run(cmd, cwd=template_path)
