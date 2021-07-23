31d69e7cdbf5503f9ce38f762211e9b1db2b56f6
Jesang Yoon <yoonjs2@gmail.com>
2021-07-24 03:18:16 +0900

    Fix iam policy

--- run_create_eb_iam.py
+++ run_create_eb_iam.py
@@ -7,11 +7,31 @@ aws_cli = AWSCli()
 
 def create_iam_profile_for_ec2_instances(template_path, name):
     profile_name = f'aws-elasticbeanstalk-{name}-instance-profile'
+    role_name = f'aws-elasticbeanstalk-{name}-ec2-role'
+    policy_name = f'aws-elasticbeanstalk-{name}-ec2-policy'
+    policy_file_path = f'file://{template_path}/{name}/_provisioning/iam/aws-elasticbeanstalk-ec2-policy.json'
+
+    cmd = ['iam', 'get-instance-profile']
+    cmd += ['--instance-profile-name', profile_name]
+    result = aws_cli.run(cmd, ignore_error=True)
+
+    if result:
+        role = result['InstanceProfile']['Roles'][0]
+        if role['RoleName'] != role_name:
+            raise Exception()
+
+        cmd = ['iam', 'put-role-policy']
+        cmd += ['--role-name', role_name]
+        cmd += ['--policy-name', policy_name]
+        cmd += ['--policy-document', policy_file_path]
+        aws_cli.run(cmd)
+
+        return profile_name, role['Arn']
+
     cmd = ['iam', 'create-instance-profile']
     cmd += ['--instance-profile-name', profile_name]
     aws_cli.run(cmd)
 
-    role_name = f'aws-elasticbeanstalk-{name}-ec2-role'
     role_file_path = f'file://{template_path}/{name}/_provisioning/iam/aws-elasticbeanstalk-ec2-role.json'
     cmd = ['iam', 'create-role']
     cmd += ['--role-name', role_name]
@@ -34,8 +54,6 @@ def create_iam_profile_for_ec2_instances(template_path, name):
     cmd += ['--policy-arn', 'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore']
     aws_cli.run(cmd)
 
-    policy_name = f'aws-elasticbeanstalk-{name}-ec2-policy'
-    policy_file_path = f'file://{template_path}/{name}/_provisioning/iam/aws-elasticbeanstalk-ec2-policy.json'
     cmd = ['iam', 'put-role-policy']
     cmd += ['--role-name', role_name]
     cmd += ['--policy-name', policy_name]
