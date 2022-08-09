186df0db1ac75432b3b7a3442b55a70d5e9f05c1
metocks <metocks@users.noreply.github.com>
2022-08-09 22:39:43 +0900

    errata

--- run_create_ramiel_codedeploy.py
+++ run_create_ramiel_codedeploy.py
@@ -33,7 +33,7 @@ print_message('create a user for codedeploy')
 iam_user_name = 'ramiel_codedeploy_iam_session_user'
 iam_role_name = 'ramiel_codedeploy_iam_session_role'
 iam_policy_name = 'ramiel_codedeploy_iam_session_permission'
-cc = ['aws', 'iam', 'create-user']
+cc = ['iam', 'create-user']
 cc += ['--user-name', iam_user_name]
 aws_cli.run(cc)
 
