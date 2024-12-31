d1567c7bb6825690beb068a42f4d6d189db28928
Jesang Yoon <yoonjs2@gmail.com>
2024-12-31 15:22:29 +0900

    update platform version

--- run_create_eb_django.py
+++ run_create_eb_django.py
@@ -460,7 +460,7 @@ def run_create_eb_django(name, settings, options):
     cmd += ['--cname-prefix', cname]
     cmd += ['--environment-name', eb_environment_name]
     cmd += ['--option-settings', option_settings]
-    cmd += ['--solution-stack-name', '64bit Amazon Linux 2023 v4.3.1 running Python 3.12']
+    cmd += ['--solution-stack-name', '64bit Amazon Linux 2023 v4.3.2 running Python 3.12']
     cmd += ['--tags', tag0, tag1]
     cmd += ['--version-label', eb_environment_name]
     aws_cli.run(cmd, cwd=template_path)
