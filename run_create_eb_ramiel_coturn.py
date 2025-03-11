656267211689aecbc13c8e2acc3990b08e15426a
Jesang Yoon <yoonjs2@gmail.com>
2025-03-11 18:32:42 +0900

    DEV-22779 update version

--- run_create_eb_ramiel_coturn.py
+++ run_create_eb_ramiel_coturn.py
@@ -407,7 +407,7 @@ def run_create_eb_ramiel_coturn(name, settings, options):
     cmd += ['--cname-prefix', cname]
     cmd += ['--environment-name', eb_environment_name]
     cmd += ['--option-settings', option_settings]
-    cmd += ['--solution-stack-name', '64bit Amazon Linux 2023 v4.4.0 running Python 3.12']
+    cmd += ['--solution-stack-name', '64bit Amazon Linux 2023 v4.4.1 running Python 3.12']
     cmd += ['--tags', tag0, tag1]
     cmd += ['--version-label', eb_environment_name]
     aws_cli.run(cmd, cwd=template_path)
